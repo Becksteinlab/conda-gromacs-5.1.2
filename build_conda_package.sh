846c073f000994562d56e1ee8cbe2efb20275bb9
Ian Kenney <ian.kenney@asu.edu>
2016-07-14 21:59:44 -0700

    Changed delimiters in sed commands

--- build_conda_package.sh
+++ build_conda_package.sh
@@ -5,7 +5,7 @@ conda-build .
 
 OUT=$(conda-build . --output)
 
-INSTALL_DIR=$(echo $(dirname $OUT)/$(basename $OUT | cut -d'.' -f1-5) | sed "s?conda-bld/linux-64?pkgs?")
+INSTALL_DIR=$(echo $(dirname $OUT)/$(basename $OUT | cut -d'.' -f1-5) | sed "s|conda-bld/linux-64|pkgs|")
 
 echo $INSTALL_DIR
 
@@ -19,9 +19,9 @@ cp $OUT /root/
 
 cp -r /tmp/gromacs_build/versions/gromacs-5.1.2/gnu/* .
 
-sed -i "s?.*GMXRC.bash.*?. $INSTALL_BIN/GMXRC.bash?" bin/GMXRC
-sed -i "s?.*GMXRC.csh.*?. $INSTALL_BIN/GMXRC.csh?" bin/GMXRC
-sed -i "s?GMXPREFIX=.*?GMXPREFIX=$INSTALL_DIR?" bin/GMXRC.bash
+sed -i "s|.*GMXRC.bash.*|. $INSTALL_BIN/GMXRC.bash|" bin/GMXRC
+sed -i "s|.*GMXRC.csh.*|. $INSTALL_BIN/GMXRC.csh|" bin/GMXRC
+sed -i "s|GMXPREFIX=.*|GMXPREFIX=$INSTALL_DIR|" bin/GMXRC.bash
 
 bunzip2 *.bz2
 tar rvf $(ls *.tar) share/

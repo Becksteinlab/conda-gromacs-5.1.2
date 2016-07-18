efb6a5892c1269593c7d4553f62eb27bbe4fec3c
Ian Kenney <ian.kenney@asu.edu>
2016-07-18 14:27:58 -0700

    Added sed commands to modify paths for GMXRC.* files

--- gromacs/build_conda_package.sh
+++ gromacs/build_conda_package.sh
@@ -15,8 +15,10 @@ cp -r /tmp/gromacs_build/versions/gromacs-5.1.2/gnu/* /root/gromacs
 
 sed -i "s|.*GMXRC.bash.*|. $INSTALL_BIN/GMXRC.bash|" bin/GMXRC
 sed -i "s|.*GMXRC.csh.*|. $INSTALL_BIN/GMXRC.csh|" bin/GMXRC
-sed -i "s|GMXPREFIX=.*|GMXPREFIX=$INSTALL_DIR|" bin/GMXRC.bash
 
+sed -i "s|GMXPREFIX=.*|GMXPREFIX=$INSTALL_DIR|" bin/GMXRC.bash
+sed -i "s|setenv GMXPREFIX.*|setenv GMXPREFIX $INSTALL_DIR|" bin/GMXRC.csh
+sed -i "s|source.*|source $INSTALL_DIR/bin/GMXRC.bash|" bin/GMXRC.zsh
 
 cd ..
 rm gromacs/*.sh gromacs/meta.yaml
